f9c8f981be2674387a9bd32ed02a4c7ef8e7aad7
Kdixitji <kdixitji@gmail.com>
2023-07-15 23:48:24 +0530

    Desktop 2

---
+++ src/Routes/Screen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Scontent from "../Screens/Scontent";
+
+const Screen = () => {
+  return (
+    <>
+      <Scontent />
+    </>
+  );
+};
+
+export default Screen;
